b2e06cacda58c2a4ae9c167a4219100c2ca9b3c9
danny <danny.tab@qq.com>
2018-10-23 16:41:33 +0800

    use ssh to remote backup git repo

---
+++ bin/backup_codeserver.sh
@@ -0,0 +1,15 @@
+codeserver=root@192.168.8.250
+remote_path=/home/git-data/repositories/
+repos=`ssh $codeserver "find $remote_path -name "*.git""`
+echo "repos=$repos"
+for repo in $repos
+do
+	echo "repo=$repo"
+	name=`basename $repo`
+	echo "name=$name"
+	if [ ! -d $name ];then
+		git clone --bare $codeserver:$repo $name
+	else
+		git fetch $name
+	fi
+done
